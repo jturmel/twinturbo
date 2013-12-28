03c8d45232428e684e618b7a9b2da604e78d77fa
Josh Turmel <jturmel@gmail.com>
2013-12-28 00:19:21 -0600

    TokenFilter classes, rework new Field
    * Setup an abstract TokenFilter class
    * Create an EdgeNGramTokenFilter based on TokenFilter
    * Setup TextField on search module that accepts an instance of TokenFilter to
      perform custom token filtering
    * Rewrite tests to accomdate changes

--- search.py
+++ search.py
@@ -1,36 +1,67 @@
-def find_ngrams(word, n):
-    word_perms = [word[i:] for i in range(n)]
-    return zip(*word_perms)
+from abc import ABCMeta
+from abc import abstractmethod
+
+from google.appengine.api import search
 
 
-def find_allngrams(word, min=1, max=None):
-    grams = []
-    for i in range(min - 1, max or len(word)):
-        grams.append(''.join(find_ngrams(word, i + 1)[0]))
+class TokenFilter(object):
+    __metaclass__ = ABCMeta
 
-    return grams
+    @abstractmethod
+    def create(self, token_string):
+        return token_string
 
 
-#class NGramField(search.TextField):
-    #"""A Field that has text content and will will be stored as n-grams
-    #starting at 2
+class EdgeNGramTokenFilter(TokenFilter):
 
-    #The following example shows a text field named first_name:
-    #TextField(name='first_name', value='Robert')
+    def __init__(self, min_gram_size=1, max_gram_size=None):
+        self.min_gram_size = min_gram_size
+        self.max_gram_size = max_gram_size
 
-    #The value will be stored as 'Ro Rob Robe Rober Robert'
-    #"""
+    def create(self, token_string):
+        edge_ngrams = self._find_edge_ngrams(token_string,
+                                             self.min_gram_size,
+                                             self.max_gram_size)
 
-    #def __init__(self, name, value=None):
-        #"""Initializer.
+        return ' '.join(edge_ngrams)
 
-        #Args:
-        #name: The name of the field.
-        #value: A str or unicode object containing text.
+    def _find_ngrams(self, word, n):
+        word_perms = [word[i:] for i in range(n)]
+        return zip(*word_perms)
 
-        #Raises:
-        #TypeError: If value is not a string.
-        #ValueError: If value is longer than allowed.
-        #"""
-        #value = find_allngrams(value, 2)
-        #super(NGramField, self).__init__(name, value)
+    def _find_edge_ngrams(self, word, min=1, max=None):
+        grams = []
+        for i in range(min - 1, max or len(word)):
+            grams.append(''.join(self._find_ngrams(word, i + 1)[0]))
+
+        return grams
+
+
+class TextField(search.TextField):
+    """A Field that has text content and will will be stored as n-grams
+    starting at 2
+
+    The following example shows a text field named first_name:
+    TextField(name='first_name', value='Robert')
+
+    The value will be stored as 'Ro Rob Robe Rober Robert'
+    """
+    def __init__(self, name, value=None, language=None, token_filter=None):
+        """Initializer.
+
+        Args:
+        name: The name of the field.
+        value: A str or unicode object containing text.
+        language: The code of the language the value is encoded in.
+        token_filter: The TokenFilter class to perform custom token filtering
+                      before storing the value
+
+        Raises:
+        TypeError: If value is not a string.
+        ValueError: If value is longer than allowed.
+        """
+        value = token_filter.create(value)
+
+        super(TextField, self).__init__(name,
+                                        search.search._ConvertToUnicode(value),
+                                        language)
--- tests/test_search.py
+++ tests/test_search.py
@@ -3,8 +3,28 @@ import search
 
 
 class SearchTest(unittest.TestCase):
-    def test_find_allngrams(self):
-        word = 'horses'
-        result = ['ho', 'hor', 'hors', 'horse', 'horses']
 
-        self.assertEqual(search.find_allngrams(word, 2), result)
+    def test_TextField1(self):
+        token_filter = search.EdgeNGramTokenFilter(min_gram_size=1)
+        field = search.TextField(name='title',
+                                 value='horses',
+                                 token_filter=token_filter)
+
+        self.assertEqual(field.value, 'h ho hor hors horse horses')
+
+    def test_TextField2(self):
+        token_filter = search.EdgeNGramTokenFilter(min_gram_size=2)
+        field = search.TextField(name='title',
+                                 value='buggies',
+                                 token_filter=token_filter)
+
+        self.assertEqual(field.value, 'bu bug bugg buggi buggie buggies')
+
+    def test_TextField3to5(self):
+        token_filter = search.EdgeNGramTokenFilter(min_gram_size=3,
+                                                   max_gram_size=5)
+        field = search.TextField(name='title',
+                                 value='buggies',
+                                 token_filter=token_filter)
+
+        self.assertEqual(field.value, 'bug bugg buggi')
